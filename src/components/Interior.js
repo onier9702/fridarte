eb33dcae41509c6c64e9a4ed0929e5d1ccd82745
onier9702 <onier0217@gmail.com>
2022-05-01 13:08:38 -0400

    Proving Photos

--- src/components/Interior.js
+++ src/components/Interior.js
@@ -32,9 +32,9 @@ export const Interior = () => {
                   <>
                     <div id="tarjeta">
                       <div id="cuadro">
-                        <img id="photo" key={p} src={`./assets${p}.jpg`} style={{maxWidth: 130}} alt=''/>
+                        <img id="photo" key={p} src={`.././assets${p}.jpg`} style={{maxWidth: 130}} alt=''/>
                       </div>
-                      <Link to={`../../single/${p}`} id="link" ><pre id="more">...</pre></Link>
+                      <Link to={`/single/${p}`} id="link" ><pre id="more">...</pre></Link>
                     </div>
                   </>
                   )
