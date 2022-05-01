5c71386d0e42122d4a3c65e352e4d3bb48a0ba84
onier9702 <onier0217@gmail.com>
2022-05-01 13:46:41 -0400

    OMG

--- src/components/Exterior.js
+++ src/components/Exterior.js
@@ -31,7 +31,7 @@ export const Exterior = () => {
                   <>
                     <div id="tarjeta">
                       <div id="cuadro">
-                        <img id="photo" key={p} src={`./assets/${p}.jpg`} style={{maxWidth: 100}} alt=''/>
+                        <img id="photo" key={p} src={`/assets/${p}.jpg`} style={{maxWidth: 100}} alt=''/>
                       </div>
                       <Link to={`/single/${p}`} id="link" ><pre id="more">...</pre></Link>
                     </div>
