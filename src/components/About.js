01c8b5dc5538147155fab8faedb011d5590f3d4e
onier9702 <onier0217@gmail.com>
2022-05-07 18:03:55 -0400

    Paragraph fixed

--- src/components/About.js
+++ src/components/About.js
@@ -9,23 +9,14 @@ export const About = () => {
           <h1 >About</h1>
           <hr />
         </div>
-        <div className="mision">
-            <h4 ><i className="bi bi-check-lg"></i>  Misión</h4>
-            <p> Brindar un servicio fotográfico exclusivo a toda persona 
-                natural o jurídica que lo solicite, utilizando una tecnología y 
-                capacitación profesional. Marcando un antes y un después en la 
-                provincia de Sancti Spíritus.
-            </p>
-
-          </div>
 
           <div className="mision">
             <h4 ><i className="bi bi-check-lg"></i>  Visión</h4>
             <p>
-              FRIDARTE STUDIO es un emprendimiento en el centro de Cuba que ofrece 
+              FRIDARTE STUDIO es una empresa en el centro de Cuba que ofrece 
               servicios múltiples de publicidad, fotografía social, empresarial y de moda
               a todas las personas naturales o jurídicas que prefieren una nueva estética 
-              visual, productos de mejor factura en todos los formatos y le interesa la 
+              visual y productos de mejor factura en todos los formatos. Está abierta a la 
               colaboración entre proyectos preferentemente de nuestra localidad.
             </p>
 
