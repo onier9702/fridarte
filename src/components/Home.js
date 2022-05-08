61bb5a5cacecf8180e5bcc64d593435e700706bc
onier9702 <onier0217@gmail.com>
2022-05-08 16:45:04 -0400

    One effect added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { getPortrait } from '../helpers/getPortrait';
 // import '../index.css';
 import './Home.css';
@@ -8,14 +8,45 @@ export const Home = () => {
 
   const portraits = useMemo(() => getPortrait('portada',13), []);
   
+  
+  let intervalScroll;
+  const startScroll = () => {
+  
+    intervalScroll = setInterval(() => {
+        window.scrollBy(0,4);
+        if ( window.innerHeight + window.scrollY === document.body.offsetHeight ){
+            stopScroll();
+        }
+    }, 20); 
 
-  console.log(portraits);
+    return intervalScroll;
+    
+  }
+  startScroll();
+
+  const stopScroll = () => {
+
+    clearInterval(intervalScroll);
+  }
+
+  document.body.addEventListener( 'click', (e) => {
+    e.preventDefault();
+    stopScroll();
+  } )
+  
+  
+
+  
+
+  
 
   return (
       <>
         {/* <div className="probando"> */}
         <div className="logo">
           <img className="logofridarte" src={process.env.PUBLIC_URL + "/assets/oficial_color.png"} alt="logo-fridarte"/>
+          <p className="parr1">Fridarte</p>
+          <p className="parr2">Studio</p>
         </div>
         {/* </div> */}
         
