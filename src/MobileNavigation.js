76f19ac87830ecdaf164c8b370a570c48644e75f
onier9702 <onier0217@gmail.com>
2022-05-08 18:39:01 -0400

    Other Commit

--- src/MobileNavigation.js
+++ src/MobileNavigation.js
@@ -10,19 +10,38 @@ export const MobileNavigation = () => {
 
     const burgerIcon = <i class="bi bi-list" onClick={ () => setOpen(!open) }></i>
     const closerIcon = <i class="bi bi-x" onClick={ () => setOpen(!open) }></i>
+    // const inst       = <i className="bi bi-instagram" style={{fontSize:5, opacity:0.8}} ></i>
+    // const what       = <i className="bi bi-whatsapp" style={{fontSize:5, opacity:0.8}}></i>
+    // const face       = <i className="bi bi-facebook" style={{fontSize:5, opacity:0.8}}></i>
+
+    const unList = <ul>
+                      <li>
+                      <i className="bi bi-instagram" style={{fontSize:15, opacity:0.5}} ></i>
+                      </li>
+                      <li>
+                      <i className="bi bi-whatsapp" style={{fontSize:15, opacity:0.5}} ></i>
+                      </li>
+                      <li>
+                      <i className="bi bi-facebook" style={{fontSize:15, opacity:0.5}} ></i>
+                      </li>
+                   </ul>
 
     const closeMobileMenu = () => setOpen(false);
 
   return (
-       
-    <div className="mobileNav" style={ open ? {backgroundColor:'black'} : {backgroundColor:'black'} } >
-        <div className="icon">
+    <>
+      <div className="three-icons">
+            { (open === false) ? unList : '' }
+      </div>
+      <div className="mobileNav" style={ open ? {backgroundColor:'black'} : {backgroundColor:'black'} } >
+          <div className="icon">
             { open ? closerIcon : burgerIcon }
-        </div>
-        <div className="nav" >
-            { open && <NavLinks isMob={true} closeMobileMenu={closeMobileMenu} /> }
-        </div>
-    </div>
+          </div>
+          <div className="nav" >
+              { open && <NavLinks isMob={true} closeMobileMenu={closeMobileMenu} /> }
+          </div>
+      </div>
+    </>
   )
 }
 
