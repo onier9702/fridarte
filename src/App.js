91f30a553a434580588dd6da1728149d62334d21
onier9702 <onier0217@gmail.com>
2022-07-26 15:44:36 -0700

    Docs again

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import { WebRouter } from './routers/WebRouter';
 
 import './index.css';
 import store from './store/store';
-
-console.log(process.env);
+import { WebRouter } from './routers/WebRouter';
 
 export const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import 'dotenv/config';
 import { HashRouter } from 'react-router-dom';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import './index.css';
 import { App } from './App';
